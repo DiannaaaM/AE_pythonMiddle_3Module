ceff739ee1f14ae82a1256983636c73a73fba13c
DiannaaaMm <murlaevadiana@gmail.com>
2025-03-26 11:53:05 +0300

    add collision

--- lesson_17/main.py
+++ lesson_17/main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import pytmx
 
+# from lesson_17.map import render_map, check_water_collision
 from lesson_17.npc import render_npc, render_text
 from player import (
     player_anim_up,
@@ -45,9 +46,11 @@ player_rect = pygame.Rect(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2, 32, 32)
 playerx_speed = 0
 playery_speed = 0
 
+
 def movement_player(x_speed, y_speed):
     player_rect.x += x_speed
     player_rect.y += y_speed
+
 
 def movement_camera(player_rect, map_width, map_height):
     camera_x = -player_rect.x + WINDOW_WIDTH // 2
@@ -60,14 +63,17 @@ def movement_camera(player_rect, map_width, map_height):
 
     return camera_x, camera_y
 
+
 # Начальные координаты NPC
 npc_x = 100  # Фиксированная позиция по оси X
 npc_y = 150  # Фиксированная позиция по оси Y
 npc_image = pygame.image.load(f'images/player/стоит/1.png').convert_alpha()  # Загрузка изображения NPC
 
+
 # Функция для отрисовки NPC
 def render_npc(surface):
     surface.blit(npc_image, (npc_x + camera_x, npc_y + camera_y))  # Рендерим NPC на фиксированной позиции
+
 
 # Функция для отрисовки текста
 def render_text(surface, text):
@@ -75,21 +81,54 @@ def render_text(surface, text):
     text_surface = font.render(text, True, (255, 255, 255))
     surface.blit(text_surface, (0, 0))
 
+# Функция для проверки столкновения коллизии игрока и тайла
+def check_water_collision(tile_rect):
+    if player_rect.colliderect(tile_rect):
+        # Если правая сторона игрока пересекает левую сторону тайла
+        if player_rect.right > tile_rect.left and player_rect.left < tile_rect.left:
+            player_rect.right = tile_rect.left  # "выталкиваем" влево (заменяем правую границу игрока)
+
+        # Если верхняя сторона игрока пересекает нижнюю сторону тайла
+        elif player_rect.top < tile_rect.bottom and player_rect.bottom > tile_rect.bottom:
+            player_rect.top = tile_rect.bottom  # Установка верхней стороны игрока на нижнюю сторону тайла
+
+        # Если левая сторона игрока пересекает правую сторону тайла
+        elif player_rect.left < tile_rect.right and player_rect.right > tile_rect.right:
+            player_rect.left = tile_rect.right  # "выталкиваем" вправо (заменяем левую границу игрока)
+
+        # Если нижняя сторона игрока пересекает верхнюю сторону тайла
+        elif player_rect.bottom > tile_rect.top and player_rect.top < tile_rect.top:
+            player_rect.bottom = tile_rect.top  # Установка нижней стороны игрока на верхнюю сторону тайла
+
+
 # Main game loop
 while True:
     window.fill(NEON_BLUE)  # Заливаем окно цветом NEON_BLUE
 
-    # Отрисовка карты
+    # Перебор видимых слоев карты
     for layer in map.visible_layers:
-        if isinstance(layer, pytmx.TiledTileLayer):
-            for x, y, gid in layer:
-                if gid:
-                    tile = map.get_tile_image_by_gid(gid)
-                    if tile:
-                        window.blit(tile, ((x * map.tilewidth) + camera_x, (y * map.tileheight) + camera_y))
+        # Перебор координат и идентификаторов тайлов в слое
+        for x, y, gid in layer:
+            # Получение изображения тайла по его глобальному идентификатору (GID)
+            tile = map.get_tile_image_by_gid(gid)
+            # Создание прямоугольника для позиционирования тайла на экране
+            tile_rect = pygame.Rect(x * map.tilewidth + camera_x, y * map.tileheight + camera_y, map.tilewidth,
+                                    map.tileheight)
 
+            # Проверка столкновений с водой, если текущий слой - вода и тайл существует (gid != 0)
+            if layer.name == 'вода' and gid != 0:
+                check_water_collision(tile_rect)
+
+            # Проверка на существование тайла перед отрисовкой
+            if tile:
+                # Отрисовка тайла на экране в окне приложения
+                window.blit(tile, tile_rect)
+
+    # Проверка коллизии с текущим тайлом
+    check_water_collision(tile_rect)
     # Draw player using the render_player function
-    render_player(pygame.Rect(player_rect.x + camera_x, player_rect.y + camera_y, player_rect.width, player_rect.height))
+    render_player(
+        pygame.Rect(player_rect.x + camera_x, player_rect.y + camera_y, player_rect.width, player_rect.height))
 
     for event in pygame.event.get():
         # Проверка на закрытие окна
@@ -122,6 +161,5 @@ while True:
     # Рендер текста
     render_text(window, "эээээЭЭЭ")  # Рендерим строку на экране
     render_npc(window)  # Рендерим NPC
-
     pygame.display.update()  # Обновляем экран
     clock.tick(FPS)  # Ограничиваем FPS
--- lesson_17/map.py
+++ lesson_17/map.py
@@ -1,53 +1,29 @@
-from lesson_15.player import player_rect
-from settings import *
-import pytmx  # для работы с картой
-
-# Загрузка карты TMX
-map = pytmx.load_pygame("maps/карта.tmx")
-
-
-def render_map():
-    # ---------- Отрисовка карты TMX--------------#
-    # Для каждого слоя карты
-    for layer in map.visible_layers:
-        # Для координат и id тайла в слое
-        for x, y, gid in layer:
-            # получаем изображения тайла
-            tile = map.get_tile_image_by_gid(gid)
-            # Если тайл определился (т.е. существует)
-            if tile:
-                # отрисовываем тайл в окне
-                window.blit(tile, (x * map.tilewidth, y * map.tileheight))
-
-
-# Функция для проверки столкновения коллизии игрока и тайла
-def check_water_collision(tile_rect):
-    if player_rect.colliderect(tile_rect):
-        # Если правая сторона игрока пересекает левую сторону тайла
-        if player_rect.right > tile_rect.left and player_rect.left < tile_rect.left:
-            player_rect.right = tile_rect.left  # "выталкиваем" влево (заменяем правую границу игрока)
-
-        # Если верхняя сторона игрока пересекает нижнюю сторону тайла
-        elif player_rect.top < tile_rect.bottom and player_rect.bottom > tile_rect.bottom:
-            player_rect.top = tile_rect.bottom  # Установка верхней стороны игрока на нижнюю сторону тайла
-
-
-def render_map(camera_x, camera_y):
-    #---------- Отрисовка карты TMX --------------#
-    # Перебор видимых слоев карты
-    for layer in map.visible_layers:
-        # Перебор координат и идентификаторов тайлов в слое
-        for x, y, gid in layer:
-            # Получение изображения тайла по его глобальному идентификатору (GID)
-            tile = map.get_tile_image_by_gid(gid)
-            # Создание прямоугольника для позиционирования тайла на экране
-            tile_rect = pygame.Rect(x * map.tilewidth + camera_x, y * map.tileheight + camera_y, map.tilewidth, map.tileheight)
-
-            # Проверка столкновений с водой, если текущий слой - вода и тайл существует (gid != 0)
-            if layer.name == 'вода' and gid != 0:
-                check_water_collision(tile_rect)
-
-            # Проверка на существование тайла перед отрисовкой
-            if tile:
-                # Отрисовка тайла на экране в окне приложения
-                window.blit(tile, tile_rect)
+# from lesson_15.player import player_rect
+# from settings import *
+# import pytmx  # для работы с картой
+#
+# # Загрузка карты TMX
+# map = pytmx.load_pygame("maps/карта.tmx")
+#
+#
+# def render_map(camera_x, camera_y):
+#     # ---------- Отрисовка карты TMX --------------#
+#     # Перебор видимых слоев карты
+#     for layer in map.visible_layers:
+#         # Перебор координат и идентификаторов тайлов в слое
+#         for x, y, gid in layer:
+#             # Получение изображения тайла по его глобальному идентификатору (GID)
+#             tile = map.get_tile_image_by_gid(gid)
+#             # Создание прямоугольника для позиционирования тайла на экране
+#             tile_rect = pygame.Rect(x * map.tilewidth + camera_x, y * map.tileheight + camera_y, map.tilewidth,
+#                                     map.tileheight)
+#
+#             # Проверка столкновений с водой, если текущий слой - вода и тайл существует (gid != 0)
+#             if layer.name == 'вода' and gid != 0:
+#                 check_water_collision(tile_rect)
+#
+#             # Проверка на существование тайла перед отрисовкой
+#             if tile:
+#                 # Отрисовка тайла на экране в окне приложения
+#                 window.blit(tile, tile_rect)
+#
